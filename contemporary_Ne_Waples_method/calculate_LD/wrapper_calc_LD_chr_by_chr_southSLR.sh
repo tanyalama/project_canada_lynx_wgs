9a314b733717fb60b4ce63c5e9d854cd8760bb7f
Tanya Lama <36007010+ECOtlama@users.noreply.github.com>
2020-07-17 12:12:23 -0400

    Create wrapper_calc_LD_chr_by_chr_southSLR.sh

---
+++ contemporary_Ne_Waples_method/calculate_LD/wrapper_calc_LD_chr_by_chr_southSLR.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+#This script is for the autosomes (chr 1 to 66)
+
+individuals_array=(NC_044317.1 NC_044318.1 NC_044319.1 NC_044320.1 NC_044321.1)
+
+for i in "${individuals_array[@]}"
+do
+
+plink --vcf  /project/uma_lisa_komoroske/Tanya/Rgenomics/mLynCan4_v1.p_lynxonly/southSLR/6SV_southSLR.vcf.gz --r2 square 'yes-really' --out /project/uma_lisa_komoroske/Tanya/scripts/Ne_Waples/work/calculate_LD/southSLR/southSLR_${i} --chr ${i} --make-bed --allow-extra-chr
+
+done
