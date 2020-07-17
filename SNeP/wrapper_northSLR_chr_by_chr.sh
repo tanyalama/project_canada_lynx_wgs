f5164d749217ab3d89b9a46ec7faea734db2464b
Tanya Lama <36007010+ECOtlama@users.noreply.github.com>
2020-07-17 12:01:17 -0400

    Create wrapper_northSLR_chr_by_chr.sh

---
+++ SNeP/wrapper_northSLR_chr_by_chr.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+# This script is for the autosomes (chr 1 to 66)
+individuals_array=( NC_044306.1 NC_044307.1 NC_044308.1 NC_044309.1 NC_044310.1 NC_044311.1 NC_044312.1 NC_044313.1 NC_044314.1 NC_044315.1 NC_044316.1 NC_044317.1 NC_044318.1 NC_044319.1 NC_044320.1 NC_044321.1 NW_022059692.1 NW_022059693.1)
+
+for i in "${individuals_array[@]}"
+do
+
+singularity exec /share/pkg/singularity/images/xenial_16.04LTS.sif /project/uma_lisa_komoroske/bin/SNeP1.1 -ped /project/uma_lisa_komoroske/Tanya/Rgenomics/mLynCan4_v1.p_lynxonly/northSLR/6SV_northSLR.ped -map /project/uma_lisa_komoroske/Tanya/Rgenomics/mLynCan4_v1.p_lynxonly/northSLR/6SV_northSLR.map -out /project/uma_lisa_komoroske/Tanya/scripts/SNeP/northSLR/northSLR_${i} -chr ${i} -samplesize 18 -itemsTH n -threads 8
+
+done
