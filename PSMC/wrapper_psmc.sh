0910c98613fc236816244a030fdfa2573e1e137e
Tanya Lama <36007010+ECOtlama@users.noreply.github.com>
2020-07-17 12:20:03 -0400

    Create wrapper_psmc.sh

---
+++ PSMC/wrapper_psmc.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Uncomment the line below and fill in the name of the individuals.
+individuals_array=(LIC60 LIC8 LIC9 LIT2 LIT5 LRK10 LRK11 LRK12 LRK13 LRK17 LRK22)
+
+for i in "${individuals_array[@]}"
+do
+
+psmc -p "4+25*2+4+6" -o ${i}.psmc ${i}.psmcfa
+done
+
+#bsub -q long -W 72:00 -R rusage[mem=8000] -n 1 "psmc -p "wrapper_psmc.sh"
